982bf88557f3b2e034433799f9d6410a624da6ed
ritikaawasthy <ritika.awasthy@gmail.com>
2021-05-01 22:11:07 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+
+
+const firebaseConfig= {
+  // Your web app's Firebase configuration
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+   apiKey: "AIzaSyA---_TIDtGuiTbOakcWpbxDqtrD_3OgVE",
+   authDomain: "evernote-clone-5a92e.firebaseapp.com",
+   projectId: "evernote-clone-5a92e",
+   storageBucket: "evernote-clone-5a92e.appspot.com",
+   messagingSenderId: "864442352251",
+   appId: "1:864442352251:web:71960dae2d1953208058de",
+   measurementId: "G-FKJTCE3FQR"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+
+
 
 ReactDOM.render(
   <React.StrictMode>
